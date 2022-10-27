993399b33a07432ac81065f8e076009c7c13d5ee
duong <pvd1303@gmail.com>
2022-10-27 18:14:49 +0700

    add network switching modal

--- js/bloodbank.js
+++ js/bloodbank.js
@@ -3,11 +3,14 @@ const MUMBAI_CHAINID = 80001;
 const BSC_CHAINID = 56;
 
 let ACTIVATED_CHAINID = MUMBAI_CHAINID;
+
 const SUPPORTED_CHAINS = [
-  POLYGON_CHAINID,
+  // POLYGON_CHAINID,
   MUMBAI_CHAINID,
-  BSC_CHAINID,
+  // BSC_CHAINID,
 ];
+
+
 
 const RPC_URL = {
   [BSC_CHAINID]: 'https://rpc.ankr.com/bsc',
@@ -15,6 +18,46 @@ const RPC_URL = {
   [MUMBAI_CHAINID]: 'https://rpc.ankr.com/polygon_mumbai',
 };
 
+const CHAINS = [
+  {
+    mainnet: true,
+    chainId: `0x${POLYGON_CHAINID.toString(16)}`,
+    rpc: RPC_URL[POLYGON_CHAINID],
+    name: "Polygon",
+    nativeCurrency: {
+        name: "MATIC",
+        symbol: "MATIC",
+        decimals: 18,
+    },
+    blockExplorer: "https://polygonscan.com",
+  },
+  {
+    mainnet: true,
+    chainId: `0x${BSC_CHAINID.toString(16)}`,
+    rpc: RPC_URL[BSC_CHAINID],
+    name: "BSC",
+    nativeCurrency: {
+        name: "BNB",
+        symbol: "BNB",
+        decimals: 18,
+    },
+    blockExplorer: "https://bscscan.com",
+  },
+  {
+    mainnet: false,
+    chainId: `0x${MUMBAI_CHAINID.toString(16)}`,
+    rpc: RPC_URL[MUMBAI_CHAINID],
+    name: "Mumbai",
+    nativeCurrency: {
+        name: "MATIC",
+        symbol: "MATIC",
+        decimals: 18,
+    },
+    blockExplorer: "https://mumbai.polygonscan.com",
+  },
+];
+
+const reloadPage = () => window.location.reload();
 const formatEther = (amountBN) => (amountBN instanceof ethers.BigNumber) ? ethers.utils.formatEther(amountBN).replace(/\.0$/, '') : 'n/a';
 const parseEther = ethers.utils.parseEther;
 const ZeroBN = ethers.constants.Zero;
@@ -38,6 +81,66 @@ const RPC_PROVIDER = {
   [MUMBAI_CHAINID]: new ethers.providers.JsonRpcProvider(RPC_URL[MUMBAI_CHAINID]),
 }
 
+const switchToNetwork = async (chainId) => {
+  const provider = vm.network.provider;
+  try {
+    await provider.request({
+      method: "wallet_switchEthereumChain",
+      params: [
+        {
+          chainId: `0x${chainId.toString(16)}`,
+        },
+      ],
+    });
+  } catch (e) {
+    let network = CHAINS.find((c) => +c.chainId == +chainId);
+    const params = [
+      {
+        chainId: network.chainId,
+        chainName: network.name,
+        nativeCurrency: network.nativeCurrency,
+        rpcUrls: [network.rpc],
+        blockExplorerUrls: [network.blockExplorer],
+      },
+    ];
+    if (e.code === 4902) {
+      await provider.request({
+        method: "wallet_addEthereumChain",
+        params,
+      });
+      await provider.request({
+        method: "wallet_switchEthereumChain",
+        params: [
+          {
+            chainId: `0x${chainId.toString(16)}`,
+          },
+        ],
+      });
+      return;
+    }
+    throw e;
+  }
+};
+
+// execute on connected to supported network 
+const connectAccount = async (web3Provider) => {
+  let accountList = await web3Provider.listAccounts();
+  if(accountList.length == 0) {
+    await web3Provider.provider.request({ method: 'eth_requestAccounts' });
+  }
+  web3Provider.provider.on("accountsChanged", (params) => {
+    console.info("metamask accountsChanged", params);
+    reloadPage();
+  });
+  const signer = web3Provider.getSigner();
+  window.provider = web3Provider;
+  window.signer = signer;
+  accountList = await web3Provider.listAccounts();
+  const walletAddress = accountList.shift();
+  await updateBalances(walletAddress);
+  vm.wallet.address = walletAddress;
+  vm.wallet.shortAddress = `${walletAddress.substr(0, 6)}..${walletAddress.substr(-4)}`;
+}
 // wallet connect functions
 const connectMetamask = async () => {
   vm.LOADING.CONNECT_WALLET = true;
@@ -45,20 +148,22 @@ const connectMetamask = async () => {
     if(vm.wallet.address) return;
     const metamaskProvider = _.get(window, 'web3.currentProvider');
     if(!metamaskProvider) throw new Error("Metamask not avaiable");
-    const provider = new ethers.providers.Web3Provider(metamaskProvider);
-    const { name, chainId, } = await provider.getNetwork();
-    let accountList = await provider.listAccounts();
-    if(accountList.length == 0) {
-      await metamaskProvider.request({ method: 'eth_requestAccounts' });
-    }
-    const signer = provider.getSigner();
-    window.provider = provider;
-    window.signer = signer;
-    accountList = await provider.listAccounts();
-    const walletAddress = accountList.shift();
-    await updateBalances(walletAddress);
-    vm.wallet.address = walletAddress;
-    vm.wallet.shortAddress = `${walletAddress.substr(0, 6)}..${walletAddress.substr(-4)}`;
+    const web3Provider = new ethers.providers.Web3Provider(metamaskProvider);
+    const { chainId } = await web3Provider.getNetwork();
+    vm.network.chainId = +chainId;
+    vm.network.provider = metamaskProvider;
+    metamaskProvider.on("chainChanged", async (chainIdHex) => {
+      console.info(`chainChanged`, +chainIdHex);
+      const chainId = +chainIdHex;
+      if(vm.wallet.address) {
+        return reloadPage();
+      } else {
+        vm.network.chainId = chainId;
+      }
+      await connectAccount(new ethers.providers.Web3Provider(metamaskProvider));
+    });
+    if(!SUPPORTED_CHAINS.includes(chainId)) return;
+    await connectAccount(web3Provider);
   } catch (exception) {
     console.error("connectMetamask error", exception);
     // throw exception;
@@ -300,6 +405,10 @@ const boostrapApp = () => {
       "The Crypt",
       "The Tomb",
     ],
+    network: {
+      chainId: undefined,
+      provider: undefined,
+    },
     activeTabId: 0,
     stakingStats: {
       global: {
@@ -420,3 +529,39 @@ const createSpinner = (classes = 'text-white') => `
   <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
 </svg>
 `.trim();
+
+const polygonSVG = `
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 38.4 33.5" class="w-20 h-20" xml:space="preserve">
+<style type="text/css">
+	.st0{fill:#8247E5;}
+</style>
+<g>
+	<path class="st0" d="M29,10.2c-0.7-0.4-1.6-0.4-2.4,0L21,13.5l-3.8,2.1l-5.5,3.3c-0.7,0.4-1.6,0.4-2.4,0L5,16.3
+		c-0.7-0.4-1.2-1.2-1.2-2.1v-5c0-0.8,0.4-1.6,1.2-2.1l4.3-2.5c0.7-0.4,1.6-0.4,2.4,0L16,7.2c0.7,0.4,1.2,1.2,1.2,2.1v3.3l3.8-2.2V7
+		c0-0.8-0.4-1.6-1.2-2.1l-8-4.7c-0.7-0.4-1.6-0.4-2.4,0L1.2,5C0.4,5.4,0,6.2,0,7v9.4c0,0.8,0.4,1.6,1.2,2.1l8.1,4.7
+		c0.7,0.4,1.6,0.4,2.4,0l5.5-3.2l3.8-2.2l5.5-3.2c0.7-0.4,1.6-0.4,2.4,0l4.3,2.5c0.7,0.4,1.2,1.2,1.2,2.1v5c0,0.8-0.4,1.6-1.2,2.1
+		L29,28.8c-0.7,0.4-1.6,0.4-2.4,0l-4.3-2.5c-0.7-0.4-1.2-1.2-1.2-2.1V21l-3.8,2.2v3.3c0,0.8,0.4,1.6,1.2,2.1l8.1,4.7
+		c0.7,0.4,1.6,0.4,2.4,0l8.1-4.7c0.7-0.4,1.2-1.2,1.2-2.1V17c0-0.8-0.4-1.6-1.2-2.1L29,10.2z"/>
+</g>
+</svg>`.trim();
+
+const bscSVG = `
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 2496 2496" class="w-20 h-20" xml:space="preserve">
+<g>
+	<path style="fill-rule:evenodd;clip-rule:evenodd;fill:#F0B90B;" d="M1248,0c689.3,0,1248,558.7,1248,1248s-558.7,1248-1248,1248
+		S0,1937.3,0,1248S558.7,0,1248,0L1248,0z"/>
+	<path style="fill:#FFFFFF;" d="M685.9,1248l0.9,330l280.4,165v193.2l-444.5-260.7v-524L685.9,1248L685.9,1248z M685.9,918v192.3
+		l-163.3-96.6V821.4l163.3-96.6l164.1,96.6L685.9,918L685.9,918z M1084.3,821.4l163.3-96.6l164.1,96.6L1247.6,918L1084.3,821.4
+		L1084.3,821.4z"/>
+	<path style="fill:#FFFFFF;" d="M803.9,1509.6v-193.2l163.3,96.6v192.3L803.9,1509.6L803.9,1509.6z M1084.3,1812.2l163.3,96.6
+		l164.1-96.6v192.3l-164.1,96.6l-163.3-96.6V1812.2L1084.3,1812.2z M1645.9,821.4l163.3-96.6l164.1,96.6v192.3l-164.1,96.6V918
+		L1645.9,821.4L1645.9,821.4L1645.9,821.4z M1809.2,1578l0.9-330l163.3-96.6v524l-444.5,260.7v-193.2L1809.2,1578L1809.2,1578
+		L1809.2,1578z"/>
+	<polygon style="fill:#FFFFFF;" points="1692.1,1509.6 1528.8,1605.3 1528.8,1413 1692.1,1316.4 1692.1,1509.6 	"/>
+	<path style="fill:#FFFFFF;" d="M1692.1,986.4l0.9,193.2l-281.2,165v330.8l-163.3,95.7l-163.3-95.7v-330.8l-281.2-165V986.4
+		L968,889.8l279.5,165.8l281.2-165.8l164.1,96.6H1692.1L1692.1,986.4z M803.9,656.5l443.7-261.6l444.5,261.6l-163.3,96.6
+		l-281.2-165.8L967.2,753.1L803.9,656.5L803.9,656.5z"/>
+</g>
+</svg>`.trim();
