f9b70b7f66ac84ec267834ebbc8460f24d5b93c6
duong <pvd1303@gmail.com>
2022-11-15 12:14:04 +0700

    update treasury.html

--- js/treasury.js
+++ js/treasury.js
@@ -1,3 +1,5 @@
+let formatter = Intl.NumberFormat('en', { notation: 'compact' });
+
 const daoWalletData = [0];
 const addRandom = (multipler) => {
   const v = Number.parseInt(Math.random() * 100 * multipler);
@@ -5,12 +7,18 @@ const addRandom = (multipler) => {
 }
 _.map(_.range(1, 31), (i) => addRandom(i));
 
-const drawCharts = () => {
-  const categories = _.map(_.range(1, 32), (i) => `${i}th`);
-  return c3.generate({
-    bindto: "#token-release-chart",
+const drawCharts1 = () => {
+  const septStartEpoch = dayjs('2022-10-01').$d.getTime();
+  const categories = _.map(_.range(1, 32), (i) => dayjs(septStartEpoch + i * 24*3600*1000).format("YYYY-MM-DD"));
+  const chart = c3.generate({
+    bindto: "#dao-treasury-balance",
     data: {
+      x: 'x',
       columns: [
+        [
+          'x',
+          ...categories,
+        ],
         [
           "DAO treasury",
           ...daoWalletData,
@@ -22,8 +30,16 @@ const drawCharts = () => {
     },
     axis: {
       x: {
-        type: 'category',
-        categories,
+        type: 'timeseries',
+        tick: {
+          // count: 4,
+          format: function (x) { return dayjs(x).format('DD MMM'); }
+        },
+      },
+      y: {
+        tick: {
+          format: x => formatter.format(x),
+        },
       }
     },
     tooltip: {
@@ -53,6 +69,96 @@ const drawCharts = () => {
     },
   });
 
+  // add background text
+  d3.select(chart.internal.config.bindto).select("svg")
+    .append("text")
+    .text("$FEAR in Treasury")
+    .attr("y","50%")
+    .attr("x","50%")
+    .style("fill", "#99999966")
+    .style("font-family", "averta")
+    .style("font-size", "2.5rem")
+    .attr("alignment-baseline", "middle")
+    .attr("text-anchor", "middle")
+    .style("pointer-events", "none")
+
+  // chart.resize({height:100, width:300})
+}
+
+const drawCharts2 = () => {
+  const septStartEpoch = dayjs('2022-10-01').$d.getTime();
+  const categories = _.map(_.range(1, 32), (i) => dayjs(septStartEpoch + i * 24*3600*1000).format("YYYY-MM-DD"));
+  const chart = c3.generate({
+    bindto: "#fear-circulation-supply",
+    data: {
+      x: 'x',
+      columns: [
+        [
+          'x',
+          ...categories,
+        ],
+        [
+          "DAO treasury",
+          ...daoWalletData,
+        ],
+      ],
+      types: {
+        "DAO treasury": "area",
+      },
+    },
+    axis: {
+      x: {
+        type: 'timeseries',
+        tick: {
+          // count: 4,
+          format: '%m/%d'
+        },
+      },
+      y: {
+        tick: {
+          format: x => formatter.format(x),
+        },
+      },
+    },
+    tooltip: {
+      format: {
+        // title: () => ``,
+        name: (name, ratio, id, index) => categories[index],
+      },
+    },
+    legend: {
+      show: false
+    },
+    // size: {
+    //   height: 240,
+    //   width: 480
+    // },
+    padding: {
+      // top: 16,
+      // right: 32,
+      // bottom: 16,
+      // left: 32,
+    },
+    color: {
+      pattern: ['#b31919'],
+    },
+    point: {
+      show: false
+    },
+  });
+
+  d3.select(chart.internal.config.bindto).select("svg")
+    .append("text")
+    .text("$FEAR circulation supply")
+    .attr("y","50%")
+    .attr("x","50%")
+    .style("fill", "#99999966")
+    .style("font-family", "averta")
+    .style("font-size", "2.5rem")
+    .attr("alignment-baseline", "middle")
+    .attr("text-anchor", "middle")
+    .style("pointer-events", "none")
+
   // chart.resize({height:100, width:300})
 }
 
@@ -64,9 +170,9 @@ const boostrapApp = () => {
       // setInterval(() => {
       //   vm.epoch = getEpoch();
       // }, 1000);
-      drawCharts();
+      drawCharts1();
+      drawCharts2();
     },
-    sdd: "testt",
     stats: {
       totalHolders: 2537,
       daoWalletBalanceBN: ethers.utils.parseEther("3156"),
