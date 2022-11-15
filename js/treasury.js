ede9e6aae29696ae5f351e6beec7e5de073121f5
duong <pvd1303@gmail.com>
2022-11-15 14:21:36 +0700

    remove fear circulation chart

--- js/treasury.js
+++ js/treasury.js
@@ -1,4 +1,17 @@
-let formatter = Intl.NumberFormat('en', { notation: 'compact' });
+const formatter = Intl.NumberFormat('en', { notation: 'compact' });
+const OneEtherBN = ethers.utils.parseEther("1");
+const OneFinneyBN = ethers.utils.parseUnits('1', 'finney');
+const formatEther = (amountBN) => (amountBN instanceof ethers.BigNumber) ? ethers.utils.formatEther(amountBN).replace(/\.0$/, '') : 'n/a';
+const formatEtherHuman = (input) => {
+  if(!(input instanceof ethers.BigNumber)) return 'n/a';
+  const OneThousandEtherBN = ethers.utils.parseEther("1000");
+  const OneMillionEtherBN = ethers.utils.parseEther("1000000");
+  if(input.gte(OneMillionEtherBN)) return formatEther(input.div(1_000_000).div(OneFinneyBN).mul(OneFinneyBN)) + "M";
+  if(input.gte(OneThousandEtherBN)) return formatEther(input.div(1_000).div(OneFinneyBN).mul(OneFinneyBN)) + "K";
+  if(input.gte(OneFinneyBN)) return formatEther(input.div(OneFinneyBN).mul(OneFinneyBN));
+  if(input.eq(ZeroBN)) return "0";
+  return "~0.001";
+}
 
 const daoWalletData = [0];
 const addRandom = (multipler) => {
@@ -162,6 +175,17 @@ const drawCharts2 = () => {
   // chart.resize({height:100, width:300})
 }
 
+const fetchAppState = async (callback) => {
+  const [
+    circulatingsupply
+  ] = await Promise.all([
+    axios.get("https://api.fear.io/api/circulatingsupply").then(r => r.data),
+  ]);
+  vm.state.circulatingsupplyBN = ethers.utils.parseEther(`${circulatingsupply}`);
+  vm.state.ready = true;
+  setTimeout(callback, 50);
+}
+
 const boostrapApp = () => {
   Alpine.store("vm", {
     epoch: null,
@@ -170,12 +194,17 @@ const boostrapApp = () => {
       // setInterval(() => {
       //   vm.epoch = getEpoch();
       // }, 1000);
-      drawCharts1();
-      drawCharts2();
+      fetchAppState(() => {
+        drawCharts1();
+      });
+      
+      // drawCharts2();
     },
-    stats: {
+    state: {
+      ready: false,
       totalHolders: 2537,
       daoWalletBalanceBN: ethers.utils.parseEther("3156"),
+      circulatingsupplyBN: null,
     },
   });
   window.vm = Alpine.store("vm");
