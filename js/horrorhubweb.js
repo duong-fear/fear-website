df7469242fd163423ff19b1a3109bd146faebfa1
duong <pvd1303@gmail.com>
2022-11-28 17:35:51 +0700

    upd

--- js/horrorhubweb.js
+++ js/horrorhubweb.js
@@ -1,6 +1,8 @@
 // config
 const googleLoginRedirectURI = undefined; //window.location.href
 
+const matic2Fear = 10.844;
+
 const loremText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
 const fetchInitialAppState = async () => {  
   sleep(0.5);
@@ -49,6 +51,9 @@ const fetchInitialAppState = async () => {
   ].map((g, index) => ({
     ...g,
     id: index,
+    // user got discount when pay with $FEAR
+    // this calculation may be wrong !!
+    priceMatic: (1.1 * g.priceFear / matic2Fear).toFixed(3),
   }))
 }
 
